520aef6ab7c1437bc00acb01a634fabc8d51d4f0
Niels Erik G. Nielsen <nielserik@indexdata.com>
2021-11-17 17:12:47 +0100

    Remove script for dropping tables
       Only one table anyway, now.

--- database/drop-tables-shared-index.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS diku_mod_reshare_index.ITEM;
-
-DROP TABLE IF EXISTS diku_mod_reshare_index.MATCH;
-
-DROP TABLE IF EXISTS diku_mod_reshare_index.BIB_RECORD;
-
-DROP TABLE IF EXISTS diku_mod_reshare_index.MATCH_VALUE;
-
-
