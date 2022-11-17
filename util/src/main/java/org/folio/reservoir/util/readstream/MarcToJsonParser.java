766a3d7c7bb053f59c5db4e266a6a25536637d18
Jakub Skoczen <jakub@indexdata.com>
2022-11-17 19:24:39 +0100

    Refactor MarcToJsonParser to use a Mapper RSRVR-94 (#24)

--- util/src/main/java/org/folio/reservoir/util/readstream/MarcToJsonParser.java
+++ util/src/main/java/org/folio/reservoir/util/readstream/MarcToJsonParser.java
@@ -1,70 +1,15 @@
 package org.folio.reservoir.util.readstream;
 
-import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.streams.ReadStream;
-import java.io.ByteArrayOutputStream;
-import org.marc4j.MarcJsonWriter;
 import org.marc4j.marc.Record;
 
-public class MarcToJsonParser implements ReadStream<JsonObject> {
+public class MarcToJsonParser extends MappingReadStream<JsonObject, Record> {
 
-  MappingReadStream<Record, Buffer> marc4jParser;
-
-  Handler<Throwable> exceptionHandler;
 
   public MarcToJsonParser(ReadStream<Buffer> stream) {
-    marc4jParser = new MappingReadStream<>(stream, new Marc4jMapper());
+    super(new MappingReadStream<>(stream, new Marc4jMapper()), new MarcToJsonMapper());
   }
 
-  @Override
-  public ReadStream<JsonObject> exceptionHandler(Handler<Throwable> handler) {
-    exceptionHandler = handler;
-    marc4jParser.exceptionHandler(handler);
-    return this;
-  }
-
-  @Override
-  public ReadStream<JsonObject> handler(Handler<JsonObject> handler) {
-    marc4jParser.handler(marcRecord -> {
-      try {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        MarcJsonWriter writer = new MarcJsonWriter(out);
-        writer.write(marcRecord);
-        JsonObject marc = new JsonObject(out.toString());
-        writer.close();
-        handler.handle(marc);
-      } catch (Exception e) {
-        if (exceptionHandler != null) {
-          exceptionHandler.handle(e);
-        }
-      }
-    });
-    return this;
-  }
-
-  @Override
-  public ReadStream<JsonObject> pause() {
-    marc4jParser.pause();
-    return this;
-  }
-
-  @Override
-  public ReadStream<JsonObject> resume() {
-    marc4jParser.resume();
-    return this;
-  }
-
-  @Override
-  public ReadStream<JsonObject> fetch(long l) {
-    marc4jParser.fetch(l);
-    return this;
-  }
-
-  @Override
-  public ReadStream<JsonObject> endHandler(Handler<Void> handler) {
-    marc4jParser.endHandler(handler);
-    return this;
-  }
 }
