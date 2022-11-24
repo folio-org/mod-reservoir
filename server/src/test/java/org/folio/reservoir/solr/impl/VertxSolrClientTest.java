a30413286a86c7e62ae20cbd1bed3a660723e622
Adam Dickmeiss <adam@indexdata.dk>
2022-11-24 11:15:29 +0100

    Using SolrContainer
    But the constructor is deprecated. Weird.

--- server/src/test/java/org/folio/reservoir/solr/impl/VertxSolrClientTest.java
+++ server/src/test/java/org/folio/reservoir/solr/impl/VertxSolrClientTest.java
@@ -1,9 +1,6 @@
 package org.folio.reservoir.solr.impl;
 
-import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
@@ -13,90 +10,37 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.handler.BodyHandler;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.MapSolrParams;
 import org.folio.reservoir.solr.VertxSolrClient;
 import org.junit.AfterClass;
-import org.junit.Assume;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.SolrContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(VertxUnitRunner.class)
 public class VertxSolrClientTest {
-  private static Vertx vertx;
-
-  private static int mockPort = 9230;
-
-  private static String solrUrl = "http://localhost:8983/solr";
-  private static String mockUrl = "http://localhost:" + mockPort + "/solr";
 
   private final static String COLLECTION = "col1";
+  
+  @ClassRule
+  public static SolrContainer solrContainer = new SolrContainer("solr:9.1.0")
+      .withCollection(COLLECTION);
 
-  private static WebClient webClient;
+  private static Vertx vertx;
 
-  private static boolean hasSolr;
+  private static String solrUrl;
 
   @BeforeClass
-  public static void beforeClass(TestContext context) {
+  public static void beforeClass() {
     vertx = Vertx.vertx();
-    webClient = WebClient.create(vertx);
-    checkSolr()
-        .onSuccess(x -> hasSolr = true)
-        .recover(x -> Future.succeededFuture())
-        .compose(x -> setupMockSolr())
-        .onComplete(context.asyncAssertSuccess());
-  }
-
-  static Future<Void> checkSolr() {
-    return webClient.getAbs(solrUrl + "/" + COLLECTION + "/select")
-        .addQueryParam("q", "title:title1")
-        .addQueryParam("wt", "json")
-        .send()
-        .map(res -> {
-          JsonObject ret = res.bodyAsJsonObject();
-          if (!ret.containsKey("responseHeader")) {
-            throw new DecodeException("Does not have responseHeader");
-          }
-          return null;
-        });
-  }
-
-  static Future<Void> setupMockSolr() {
-    Router router = Router.router(vertx);
-    router.post().handler(BodyHandler.create());
-    router.get("/solr/" + COLLECTION + "/select").handler(c -> {
-      JsonObject response = new JsonObject()
-          .put("responseHeader",
-              new JsonObject()
-                  .put("status", 0)
-          )
-          .put("response", new JsonObject()
-              .put("numFound", 1)
-          );
-      c.response().setStatusCode(200);
-      c.response().putHeader("Content-Type", "application/json");
-      c.response().end(response.encodePrettily());
-    });
-    router.post("/solr/" + COLLECTION + "/update").handler(c -> {
-      JsonObject response = new JsonObject()
-          .put("responseHeader",
-              new JsonObject()
-                  .put("status", 0)
-          );
-      c.response().setStatusCode(200);
-      c.response().putHeader("Content-Type", "application/json");
-      c.response().end(response.encodePrettily());
-    });
-    HttpServer httpServer = vertx.createHttpServer().requestHandler(router);
-    return httpServer.listen(mockPort).mapEmpty();
+    solrUrl = "http://" + solrContainer.getHost() + ":" + solrContainer.getSolrPort() + "/solr";
   }
 
   @AfterClass
@@ -106,7 +50,6 @@ public class VertxSolrClientTest {
 
   @Test
   public void addOneDocument(TestContext context) {
-    Assume.assumeTrue(hasSolr);
     final UUID docId = UUID.randomUUID();
 
     VertxSolrClientSolrj c = new VertxSolrClientSolrj(vertx, solrUrl, COLLECTION);
@@ -121,7 +64,6 @@ public class VertxSolrClientTest {
 
   @Test
   public void queryOneDocument(TestContext context) {
-    Assume.assumeTrue(hasSolr);
     final UUID docId = UUID.randomUUID();
     VertxSolrClientSolrj c = new VertxSolrClientSolrj(vertx, solrUrl, COLLECTION);
     SolrInputDocument doc = new SolrInputDocument();
@@ -143,7 +85,6 @@ public class VertxSolrClientTest {
 
   @Test
   public void addOneJsonDocumentSolrj(TestContext context) {
-    Assume.assumeTrue(hasSolr);
     final UUID docId = UUID.randomUUID();
     VertxSolrClient c = new VertxSolrClientSolrj(vertx, solrUrl, COLLECTION);
     JsonArray docs = new JsonArray()
@@ -161,7 +102,6 @@ public class VertxSolrClientTest {
 
   @Test
   public void commitSolrj(TestContext context) {
-    Assume.assumeTrue(hasSolr);
     VertxSolrClient c = new VertxSolrClientSolrj(vertx, solrUrl, COLLECTION);
     c.commit()
         .onComplete(context.asyncAssertSuccess());
@@ -169,7 +109,6 @@ public class VertxSolrClientTest {
 
   @Test
   public void searchWebClient(TestContext context) {
-    Assume.assumeTrue(hasSolr);
     final UUID docId1 = UUID.randomUUID();
     final UUID docId2 = UUID.randomUUID();
     VertxSolrClient c = VertxSolrClient.create(vertx, solrUrl, COLLECTION);
@@ -191,22 +130,4 @@ public class VertxSolrClientTest {
           assertThat(response.getJsonArray("docs").getJsonObject(0), is(docs.getJsonObject(1)));
         }));
   }
-
-  @Test
-  public void searchWebClientMock(TestContext context) {
-    final UUID docId = UUID.randomUUID();
-    VertxSolrClient c = VertxSolrClient.create(vertx, mockUrl, COLLECTION);
-    JsonArray docs = new JsonArray()
-        .add(new JsonObject()
-            .put("id", docId.toString())
-            .put("title", new JsonArray().add("title3a").add("title3b")));
-    Map<String,String> map = Map.of("q", "id:" + docId, "fl", "id,title", "sort", "id asc");
-    c.add(docs)
-        .compose(x -> c.commit())
-        .compose(x -> c.query(map))
-        .onComplete(context.asyncAssertSuccess(res -> {
-          assertThat(res.getJsonObject("response").getInteger("numFound"), is(1));
-        }));
-  }
-
 }
